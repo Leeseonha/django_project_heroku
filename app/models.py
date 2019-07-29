49f3d0bc287d1cabbc941b7e7214070804a4c7fe
Leeseonha <jmtkdsh@gmail.com>
2019-07-29 23:17:23 +0900

    first

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    body = models.TextField()
+    hashtags = models.ManyToManyField('Hashtag',blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.body[:100]
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
